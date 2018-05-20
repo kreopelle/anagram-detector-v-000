64a355d484b30d7130f9f6029c9448e2acbd922e
Kayla Reopelle <kreopelle@gmail.com>
2018-05-20 02:41:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,10 @@
 # Your code goes here!
 # given a word and list of possible angrams, returns the correct one(s)
 # create an Angram class, accepts word on initialization
-# match method - returns matches in an array, if no matches, empty array 
-# .split("") and .sort as tips 
+# match method - returns matches in an array, if no matches, empty array
+# .split("") and .sort as tips
 
+class Angram
 
+  
+end
